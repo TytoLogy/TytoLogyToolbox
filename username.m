72156f4d1a5c51588da93e25d2474c62c5db5312
Sharad Shanbhag <sshanbhag@neomed.edu>
2017-10-10 11:36:31 -0400

    updated username to also return host name

--- username.m
+++ username.m
@@ -1,19 +1,20 @@
-function [name, os_type] = username
+function [user_name, os_type, host_name] = username
 %------------------------------------------------------------------------
-% [name, os_type] = username
+% [user_name, os_type, host_name] = username
 %------------------------------------------------------------------------
 % TytoLogyTools toolbox
 %------------------------------------------------------------------------
 % 
-% returns system username and os type 
+% returns system username, os type, machine name (hostname)
 % 
 %------------------------------------------------------------------------
 % Input Arguments:
 % 	none
 % 
 % Output Arguments:
-% 	name		operating system-defined user name
+% 	user_name		operating system-defined user name
 %	os_type	operating system name
+%	host_name	system host name
 %------------------------------------------------------------------------
 % See also: computer, system, echo
 %------------------------------------------------------------------------
@@ -28,21 +29,32 @@ function [name, os_type] = username
 %	10 May, 2011:	added MACI64 as valid os_type
 %	3 May, 2012:	added MACI as valid os_type
 %	19 Apr, 2016:	added PCWIN64 as valid os_type
+%	10 Oct, 2017:	added host_name as output value
 %------------------------------------------------------------------------
 % TO DO:
 %------------------------------------------------------------------------
 
-
+% get operating system type 
 os_type = computer;
+
+% actions depend on system type
 switch os_type
-	case {'PCWIN', 'PCWIN64'}	
-		[status, name] = system('echo %UserName%'); %#ok<*ASGLU>
-		name = name(1:end-1);
+	case {'PCWIN', 'PCWIN64'}
+		% get user name
+		[~, tmp] = system('echo %UserName%');
+		user_name = tmp(1:end-1);
+		% get host name
+		[~, tmp] = system('echo %ComputerName%');
+		host_name = tmp(1:end-1);
 		
 	case {'MAC', 'MACI', 'GLNXA64', 'MACI64'}
-		[status, name] = system('whoami');
-		name = sscanf(name, '%s');
-
+		% get user name
+		[~, tmp] = system('whoami');
+		user_name = sscanf(tmp, '%s');
+		% get host name
+		[~, tmp] = system('hostname');
+		host_name = sscanf(tmp, '%s');
+		
 	otherwise
 		error([mfilename ': ' os_type ' is unknown computer'])
 end
